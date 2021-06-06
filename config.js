3479200c23435c99ffa1628d5fc33b4e552f9fad
ktd612 <85434901+ktd612@users.noreply.github.com>
2021-06-06 16:33:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnYes: 'Yêu Be thúi',
     btnNo: 'Không, lew lew :)))',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
